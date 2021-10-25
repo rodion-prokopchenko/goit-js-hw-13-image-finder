c71c6c9005fa5ac5ab86fd0bb481da25eb196539
R0DlON <rodionprokopchenkoo@gmail.com>
2021-10-25 04:46:46 +0300

    draft#5FINALL

--- src/index.js
+++ src/index.js
@@ -5,26 +5,36 @@ const fetchF = new Fetch();
 const refs = {
   searchForm: document.querySelector('.search'),
   containerGallery: document.querySelector('.gallery'),
+  loadMoreButton: document.querySelector('.load-button'),
 };
 console.log(refs.searchForm);
 
 refs.searchForm.addEventListener('input', search);
+refs.loadMoreButton.addEventListener('click', loadMore);
 
 function search(e) {
   e.preventDefault();
   fetchF.query = e.target.value;
-  clearGallery();
   console.log(fetchF.searchQuery);
+
+  clearGallery();
+  // как лучше? чтобы ---IF(...){}--- оставался в функции search(e)?
   if (e.target.value !== '') {
-    fetchF.fetchGallery().then(items => {
-      if (items.totalHits <= 0) {
-        alert('пусто');
-      }
-      return appendSearchItems(items);
-    });
+    renderItems(e);
   }
+  // или чтобы ---IF(...{})--- забирала микро-функция renderItems?
+  searchIsEmpty(e);
 
   console.log(fetchF.fetchGallery());
+}
+function loadMore() {
+  fetchF.pageNumber += 1;
+  console.log(fetchF.pageNumber);
+  renderItems();
+  refs.containerGallery.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
 }
 
 function clearGallery() {
@@ -36,7 +46,22 @@ function createSearchItems(e) {
 function appendSearchItems(e) {
   refs.containerGallery.insertAdjacentHTML('beforeend', createSearchItems(e));
 }
-function searchEmpty(e) {
-  if ((e = '')) {
+function searchIsEmpty(e) {
+  if (e.target.value === '') {
+    alert('Введите что-то');
   }
 }
+function renderItems(e) {
+  fetchF.fetchGallery().then(items => {
+    if (items.totalHits <= 0) {
+      alert('пусто');
+    }
+    return appendSearchItems(items);
+  });
+}
+
+// const element = document.querySelector('.my-element-selector');
+// element.scrollIntoView({
+//   behavior: 'smooth',
+//   block: 'end',
+// });
