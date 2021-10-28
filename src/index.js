0937fe22830ba87e61b0f4f82f98fda5da8e4a4d
R0DlON <rodionprokopchenkoo@gmail.com>
2021-10-29 00:36:53 +0300

    ready for QA

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './sass/main.scss';
 import Fetch from './partials/apiService';
 import galleryItems from './tempates/gallery.hbs';
+import debounce from 'lodash.debounce';
 const fetchF = new Fetch();
 const refs = {
   searchForm: document.querySelector('.search'),
@@ -9,59 +10,73 @@ const refs = {
 };
 console.log(refs.searchForm);
 
-refs.searchForm.addEventListener('input', search);
+refs.searchForm.addEventListener('input', debounce(search, 500));
 refs.loadMoreButton.addEventListener('click', loadMore);
 
 function search(e) {
   e.preventDefault();
   fetchF.query = e.target.value;
-  console.log(fetchF.searchQuery);
 
   clearGallery();
   // как лучше? чтобы ---IF(...){}--- оставался в функции search(e)?
   if (e.target.value !== '') {
     renderItems(e);
+    unDisableLoadButton();
   }
   // или чтобы ---IF(...{})--- забирала микро-функция renderItems?
   searchIsEmpty(e);
 
   console.log(fetchF.fetchGallery());
+  console.log(refs.containerGallery.children);
 }
 function loadMore() {
   fetchF.pageNumber += 1;
   console.log(fetchF.pageNumber);
+
   renderItems();
+  console.log(refs.containerGallery.lastElementChild);
+  setTimeout(scroll, 550);
+}
+
+function scroll() {
   refs.containerGallery.scrollIntoView({
     behavior: 'smooth',
     block: 'end',
   });
 }
 
+function unDisableLoadButton() {
+  refs.loadMoreButton.removeAttribute('disabled', true);
+}
+
+function disableLoadButton() {
+  refs.loadMoreButton.setAttribute('disabled', true);
+}
+
 function clearGallery() {
   refs.containerGallery.innerHTML = '';
 }
+
 function createSearchItems(e) {
   return galleryItems(e);
 }
+
 function appendSearchItems(e) {
   refs.containerGallery.insertAdjacentHTML('beforeend', createSearchItems(e));
 }
+
 function searchIsEmpty(e) {
   if (e.target.value === '') {
     alert('Введите что-то');
   }
 }
-function renderItems(e) {
-  fetchF.fetchGallery().then(items => {
+
+async function renderItems(e) {
+  await fetchF.fetchGallery().then(items => {
     if (items.totalHits <= 0) {
       alert('пусто');
+      disableLoadButton();
     }
     return appendSearchItems(items);
   });
 }
-
-// const element = document.querySelector('.my-element-selector');
-// element.scrollIntoView({
-//   behavior: 'smooth',
-//   block: 'end',
-// });
